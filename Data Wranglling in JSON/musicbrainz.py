3dd25df84a272351bf29cedfa87c4f27b08c89d1
Patrick <patrick@ubuntu.ubuntu-domain>
2017-09-24 18:20:44 -0300

    Resposta da primeira pergunta, talvez voltar para responder o restante

--- Data Wranglling in JSON/musicbrainz.py
+++ Data Wranglling in JSON/musicbrainz.py
@@ -21,6 +21,23 @@ query_type = {  "simple": {},
                 "aliases": {"inc": "aliases"},
                 "releases": {"inc": "releases"}}
 
+FIRST_AID_KIT = "First Aid Kit"
+
+REQUEST_FACTORY = RequestUtils(BASE_URL)
+
+
+def answerFirstAidKit():
+
+    response_first_aid_kit =REQUEST_FACTORY.query_by_name(query_type["simple"], FIRST_AID_KIT, REQUEST_FACTORY.ARTIST_URL_APPEND)
+    response_first_aid_kit_list = [band for band in response_first_aid_kit["artists"] if band["name"] == FIRST_AID_KIT]
+    answer_first_question = len(response_first_aid_kit_list)
+    pretty_print("There is {} with the name of {}".format(answer_first_question, FIRST_AID_KIT))
+
+
+def AnswerQuestion():
+
+    answerFirstAidKit()
+
 def main():
     """
     Below is an example investigation to help you get started in your
@@ -56,4 +73,4 @@ def main():
         print(t)
 
 if __name__ == '__main__':
-    main()
+    AnswerQuestion()
--- Utils/RequestUtils.py
+++ Utils/RequestUtils.py
@@ -1,24 +1,26 @@
 import requests
 
 class RequestUtils:
+    ARTIST_URL_APPEND = "artist/"
+
     def __init__(self, urlBase):
         self.urlBase = urlBase
 
-    def query_by_name(self, params, name):
+    def query_by_name(self, params, name, typeQuery = ""):
         """
         This adds an artist name to the query parameters before making an API call
         to the function above.
         """
         params["query"] = "artist:" + name
-        return self.query_site(params)
+        return self.query_site(typeQuery, params)
 
-    def query_site(self, params, uid="", fmt="json"):
+    def query_site(self, urlAppend = "", params=[], uid="", fmt="json"):
         """
         This is the main function for making queries to the musicbrainz API. The
         query should return a json document.
         """
         params["fmt"] = fmt
-        r = requests.get(self.urlBase + uid, params=params)
+        r = requests.get("{0}{1}".format(self.urlBase, urlAppend) + uid, params=params)
         print("requesting", r.url)
 
         if r.status_code == requests.codes.ok:
