b51b83495b68f11091abf3e5cb6c6b760af375f0
Patrick <patrick@ubuntu.ubuntu-domain>
2017-09-26 05:49:50 -0300

    Desafio 3

--- Utils/RequestUtils.py
+++ Utils/RequestUtils.py
@@ -3,22 +3,17 @@ import requests
 class RequestUtils:
     ARTIST_URL_APPEND = "artist/"
 
-    def __init__(self, urlBase):
+    def __init__(self, urlBase, api_key=[]):
         self.urlBase = urlBase
+        self.api_key =  api_key
 
     def query_by_name(self, params, name, typeQuery = ""):
-        """
-        This adds an artist name to the query parameters before making an API call
-        to the function above.
-        """
+
         params["query"] = "artist:" + name
         return self.query_site(typeQuery, params)
 
     def query_site(self, urlAppend = "", params=[], uid="", fmt="json"):
-        """
-        This is the main function for making queries to the musicbrainz API. The
-        query should return a json document.
-        """
+
         params["fmt"] = fmt
         r = requests.get("{0}{1}".format(self.urlBase, urlAppend) + uid, params=params)
         print("requesting", r.url)
@@ -27,3 +22,8 @@ class RequestUtils:
             return r.json()
         else:
             r.raise_for_status()
+
+    def query_site_authentication(self,target, offset, urlAppend = ""):
+
+        params = {"api-key": self.api_key[target], "offset": offset}
+        self.query_site(urlAppend, params)
