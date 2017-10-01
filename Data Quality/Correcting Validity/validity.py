68671793ad061ecb3d37092f1a44f848e4cff74b
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-01 08:34:47 -0300

    WIP good file and bad file

--- Data Quality/Correcting Validity/validity.py
+++ Data Quality/Correcting Validity/validity.py
@@ -1,26 +1,36 @@
 import csv
 import pprint
 import pdb
+import datetime
+import re
+from dateutil import parser
 
 INPUT_FILE = '../../fixtures/autos.csv'
 OUTPUT_GOOD = '../../fixtures/autos-valid.csv'
 OUTPUT_BAD = '../../fixtures/FIXME-autos.csv'
+REGEX_IS_DATE = re.compile("^\\d{4}[-]?\\d{1,2}[-]?\\d{1,2}T\\d{1,2}:\\d{1,2}:\\d{1,2}[,]?\\d{1,3}")
+
 
 def check_for_url(url):
-    pdb.set_trace()
     return "good"
 
 def check_for_year_range(year):
-    pdb.set_trace()
-    return "good"
+
+    year = parser.parse(year)
+    if(year.year >= 1886 and year.year <= 2014):
+        return "good"
+    else:
+        return "bad"
 
 def check_for_year(is_year):
-    pdb.set_trace()
-    return "good"
+    if REGEX_IS_DATE.match(is_year) != None:
+        return "good"
+    else:
+        return "bad"
 
 def row_validations(row_dict):
     row_state = check_for_year(row_dict["productionStartYear"])
-    row_state = check_for_year_range(row_dict["productionStartYear"])
+    row_state = check_for_year_range(row_dict["productionStartYear"]) if row_state == "good" else "bad"
     row_state = check_for_url(row_dict["URI"])
     return row_state
 	
@@ -37,16 +47,16 @@ def process_file(input_file, output_good, output_bad):
         header = reader.fieldnames
 
         for row in reader:
-	    row_dict = {}
+            row_dict = {}
 
             for field in header:
                 row_dict[field] = row[field]
 
-	    is_good_file = row_validations(row_dict)
-	    if is_good_file == "good" :
-		good_file.append(row_dict)
-	    elif is_good_file == "bad" :
-		bad_file.append(row_dict)
+            is_good_file = row_validations(row_dict)
+            if is_good_file == "good" :
+                good_file.append(row_dict)
+            elif is_good_file == "bad" :
+                bad_file.append(row_dict)
 
         #COMPLETE THIS FUNCTION
     pdb.set_trace()
