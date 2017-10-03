b67e6126173243893663e48aa1c5fe799fb148de
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-03 07:29:13 -0300

    Remoção de comentarios

--- Data Quality/Fixing Area/area.py
+++ Data Quality/Fixing Area/area.py
@@ -1,26 +1,15 @@
-#n this problem set you work with cities infobox data, audit it, come up with a
-#cleaning idea and then clean it up.
-
-#Since in the previous quiz you made a decision on which value to keep for the
-#"areaLand" field, you now know what has to be done.
-
-#Finish the function fix_area(). It will receive a string as an input, and it
-#has to return a float representing the value of the area or None.
-#You have to change the function fix_area. You can use extra functions if you
-#like, but changes to process_file will not be taken into account.
-#The rest of the code is just an example on how this function can be used.
-#"""
 import codecs
 import csv
 import json
 import pprint
+import pdb
 
-CITIES = 'cities.csv'
+CITIES = '../../fixtures/cities.csv'
 
 
 def fix_area(area):
 
-    # YOUR CODE HERE
+    pdb.set_trace()
 
     return area
 
