8464356404edd301150be6e3195cad430145806c
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-10 07:35:00 -0300

    Discovering the xml node tags

--- Final_Project/mapparser.py
+++ Final_Project/mapparser.py
@@ -1,22 +1,24 @@
 import xml.etree.cElementTree as ET
 import pprint
+import pdb
+
+PATH_TO_FILE = "../fixtures/example.osm"
 
 def count_tags(filename):
-        # YOUR CODE HERE
-
+    current_dict = {}
+    line_count = 0
+    for event, elem in ET.iterparse(filename):
+        line_count = line_count + 1
+        #if len([True for key in current_dict if current_dict[key] == elem.tag]) == 0:
+        if elem.tag not in current_dict:
+            current_dict[elem.tag] = line_count
+    
+    return current_dict
 
 def test():
 
-    tags = count_tags('example.osm')
+    tags = count_tags(PATH_TO_FILE)
     pprint.pprint(tags)
-    assert tags == {'bounds': 1,
-                     'member': 3,
-                     'nd': 4,
-                     'node': 20,
-                     'osm': 1,
-                     'relation': 1,
-                     'tag': 7,
-                     'way': 1}
 
     
 
